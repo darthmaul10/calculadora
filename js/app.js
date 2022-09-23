f33756edc900c0663401b32fd8a3a822b198392d
darthmaul10 <75346467+darthmaul10@users.noreply.github.com>
2022-09-22 19:09:21 -0600

    terminada

--- js/app.js
+++ js/app.js
@@ -1,163 +1,74 @@
-const carrito = document.querySelector('#exampleModal');
-const contenedorCarrito = document.querySelector('#lista-carrito tbody');
-const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
-const listaProductos = document.querySelector('#lista-productos');
-let articulosCarrito = [];
+const calc = document.querySelector('#calculadora tbody');
+const btnC = document.querySelector('#limpiar');
+const pantalla = document.querySelector('input');
+const igual = document.querySelector('#igual');
+let num1 = '';
+let num2 = '';
+let operacion = '';
 
 cargarEventListener();
 
 function cargarEventListener() {
-    //cuando precionamos el boton agregar al carrito
-    listaProductos.addEventListener('click', agregarCarrito);
 
-    //eliminar articulo del carrito
-    carrito.addEventListener('click', eliminarArticulo);
+    //cuando precionamos un boton de la calculadora
+    calc.addEventListener('click', calcular);
 
-    //vaciar carrito
-    vaciarCarritoBtn.addEventListener('click', () => {
-        articulosCarrito = [];
+    // al hacer click en btn-limpiar
+    btnC.addEventListener('click', resetear);
 
-        limpiarHTMLCarrito();
-    });
+    //al hacer click en btn =
+    igual.addEventListener('click', calcular2)
 
 }
 
-function agregarCarrito(e) {
+function calcular(e) {
+    const btn = e.target.classList;
     e.preventDefault();
 
-    //si el presionado contine producto-p
-    if (e.target.classList.contains('producto-p')) {
+    // si es un numero
+    if (btn.contains('numeros')) {
+        pantalla.value += e.target.textContent;
 
-        const productoSeleccionado = e.target.parentElement.parentElement;
-
-
-        leerDatosProductos(productoSeleccionado);
-
-
+    } else if (btn.contains('operadores')) {
+        num1 = pantalla.value;
+        operacion = e.target.textContent;
+        limpiar();
+        console.log(num1);
     }
 }
 
-//eliminar el curso del carrito
-function eliminarArticulo(e) {
-    e.preventDefault();
-
-    if (e.target.classList.contains('bi-trash')) {
-        const articuloId = e.target.parentElement.getAttribute('data-id');
-
-        //eliminar del arreglo de articulosCarrito por el data-id
-
-        articulosCarrito = articulosCarrito.filter(articulo => articulo.id !== articuloId);
-
-
-        carritoHTML();
-    }
+function calcular2(e) {
+    num2 = pantalla.value;
+    evaluar();
 }
 
-
-//lee a lo que le dimos click
-
-function leerDatosProductos(producto) {
-
-
-
-    const infoProducto = {
-        imagen: producto.querySelector('img').src,
-        nombre: producto.querySelector('h5').textContent,
-        precio: producto.querySelector('.precio').textContent,
-        id: producto.querySelector('a').getAttribute('data-id'),
-        cantidad: producto.querySelector('input').value,
-    };
-
-
-    if (producto.querySelector('input').value == '') {
-        alert('El producto debe agregarse con una cantidad diferente de 0')
-    } else {
-
-        producto.querySelector('input').value = '';
-        //revisa si un elemento ya existe en el carrito
-
-        const existe = articulosCarrito.some(articulo => articulo.id === infoProducto.id)
-
-        if (!existe) {
-            //agregando producto al carrito
-            articulosCarrito.push(infoProducto);
-        } else {
-            const articulos = articulosCarrito.map(articulo => {
-                if (articulo.id === infoProducto.id) {
-
-                    articulo.cantidad = parseInt(articulo.cantidad) + parseInt(infoProducto.cantidad);
-
-                    return articulo;
-                } else {
-                    return articulo;
-                }
-            });
-            articulosCarrito = [...articulos]
-        }
-        alert('Producto Agregado al carrito')
-        carritoHTML();
-    }
-
-
+function limpiar() {
+    pantalla.value = "";
 }
 
-//mostrar el carrito de compras
-function carritoHTML() {
-
-    //limpiar html antes de recorrer el carrito
-    limpiarHTMLCarrito()
-    let total = 0;
-
-    //recorre el carrito y genera el html
-    articulosCarrito.forEach(articulo => {
-
-        const {
-            imagen,
-            nombre,
-            precio,
-            id,
-            cantidad
-        } = articulo;
-        let costo = parseFloat(cantidad * precio).toFixed(2);
-        const fila = document.createElement('tr');
-        fila.innerHTML = `
-    <td> <img src="${imagen}" alt="imagen producto" width="100" > </td>
-    <td> ${nombre}</td>
-    <td> ${precio}</td>
-    <td> ${cantidad}</td>
-    <td>  ${costo}</td>
-    <td> <a href="#" data-id="${id}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-    <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-  </svg></a></td>
-    `;
-
-        total += parseFloat(precio).toFixed(2) * parseFloat(cantidad).toFixed(2);
-
-        //agrego el html del carrito al body de la table carrito
-        contenedorCarrito.appendChild(fila);
-    });
-
-    let total2  = parseFloat(total).toFixed(2);
-    let tax = parseFloat(total2*0.15).toFixed(2) ;
-    let total3 =(parseFloat(total2) + parseFloat(tax)).toFixed(2) ;
-
-    //agregando el total
-    let totalRow = document.createElement('tr');
-    totalRow.innerHTML = `
-    <td scope="row" > Subtotal:  ${total2}</td>
-    <td scope="row" > Impuesto:  ${tax}</td>
-    <td scope="row" > Total:  ${total3}</td>
-    `
-    contenedorCarrito.appendChild(totalRow);
-
-
-
+function resetear() {
+    pantalla.value = "";
+    num1 = 0;
+    num2 = 0;
+    operacion = "";
 }
 
-function limpiarHTMLCarrito() {
-    while (contenedorCarrito.firstChild) {
-        contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+function evaluar() {
+    let res = 0;
+    switch (operacion) {
+        case "+":
+            res = parseFloat(num1) + parseFloat(num2);
+            break;
+        case "-":
+            res = parseFloat(num1) - parseFloat(num2);
+            break;
+        case "*":
+            res = parseFloat(num1) * parseFloat(num2);
+            break;
+        case "/":
+            res = parseFloat(num1) / parseFloat(num2);
+            break;
     }
-
+    resetear();
+    pantalla.value = res;
 }
